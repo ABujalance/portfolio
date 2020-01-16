d8448874ff853fe896555507f0602fed71fbc1b1
Alberto Bujalance <alberto.bujalance@babel.es>
2020-01-16 21:01:17 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ABujalance/ABujalance.github.io/precache-manifest.d633c4d5eb5406f53e8f23761353e9c4.js"
+  "/ABujalance.github.io/precache-manifest.4f8584eb24efd243aa7f94372fd56e61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ABujalance/ABujalance.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ABujalance.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
