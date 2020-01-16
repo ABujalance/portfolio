5b254cf8d7132958361d47c17853ba5c99624c74
Alberto Bujalance <alberto.bujalance@babel.es>
2020-01-16 20:56:17 +0100

    Updates

---
+++ precache-manifest.d633c4d5eb5406f53e8f23761353e9c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a103f7736765d8e38643a1fbc5fb32",
+    "url": "/ABujalance/ABujalance.github.io/index.html"
+  },
+  {
+    "revision": "e5cdc4460472706b9418",
+    "url": "/ABujalance/ABujalance.github.io/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "8775563f26236cae13d4",
+    "url": "/ABujalance/ABujalance.github.io/static/js/2.aabefd6c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ABujalance/ABujalance.github.io/static/js/2.aabefd6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5cdc4460472706b9418",
+    "url": "/ABujalance/ABujalance.github.io/static/js/main.4d383ea6.chunk.js"
+  },
+  {
+    "revision": "ba8e768cf0e2e9649099",
+    "url": "/ABujalance/ABujalance.github.io/static/js/runtime-main.b2e4e07c.js"
+  }
+]);
